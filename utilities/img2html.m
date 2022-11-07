09b9bfca082ac8eecea9fd6276d0cff2748ab847
Ted Zhang <tedz@andrew.cmu.edu>
2022-11-07 17:34:37 -0500

    add in code to not write figures into html if deployed

--- utilities/img2html.m
+++ utilities/img2html.m
@@ -1,5 +1,11 @@
 function img2html ( fileID, imagepath, imagepatht, caption)
+    
+if isdeployed
+    
+    fprintf ( fileID, '\n');
 
+else
+    
     fprintf( fileID, '\n' );
     fprintf( fileID, '<figure>\n' );
     fprintf( fileID, ['<a href="' imagepath '"><img src="' imagepatht '" /></a>\n'] );
