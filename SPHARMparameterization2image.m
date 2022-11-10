43853fcc87fe1e1105a9bae238cff11e2887a8c6
Ted Zhang <tedz@andrew.cmu.edu>
2022-11-10 15:15:46 -0500

    add in more default options for spharmp2img

--- SPHARMparameterization2image.m
+++ SPHARMparameterization2image.m
@@ -24,6 +24,30 @@ end
 if ~isfield(options, 'debug')
     options.debug = false;
 end
+if ~isfield(options, 'figtitle')
+    options.figtitle = [];
+end
+if ~isfield(options, 'plot')
+    options.plot = 0;
+end
+if ~isfield(options, 'dpi')
+    options.dpi = 150;
+end
+if ~isfield(options, 'filename')
+    options.filename = [];
+end
+if ~isfield(options,'meshtype.type')
+    options.meshtype.type = 'triangular';
+end
+if ~isfield(options,'meshtype.nPhi')
+    options.meshtype.nPhi = 64;
+end
+if ~isfield(options,'meshtype.nTheta')
+    options.meshtype.nTheta = 32;
+end
+if ~isfield(options,'nVertices')
+    options.nVertices = 4002;
+end
 %%%%%%%%%%%%%%%%%%%%%
 
 deg = param_output.deg;
