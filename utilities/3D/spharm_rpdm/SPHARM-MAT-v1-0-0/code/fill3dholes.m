446f9ce36fe76907f0f4c0dddf80ebdb462b2e43
Ted Zhang <tedz@andrew.cmu.edu>
2023-03-14 10:01:56 -0400

    wrap fill3dhole figures in isdeployed so that it only generates the figure during matlab runs

--- utilities/3D/spharm_rpdm/SPHARM-MAT-v1-0-0/code/fill3dholes.m
+++ utilities/3D/spharm_rpdm/SPHARM-MAT-v1-0-0/code/fill3dholes.m
@@ -127,42 +127,44 @@ end
 
 fprintf('original %d, after_filling %d, difference %d\n',sum(X(:)),sum(Y(:)),length(find((Y-X))));
 
-% figure;
-% 
-% subplot(2,2,1); [vertices, faces] = gensfdata(X);
-% patches = patch('faces', faces, 'vertices', vertices, ...
-% 			'FaceVertexCData', ones(size(vertices,1),1)*[0.8 0.8 0.8], ...
-% 			'FaceColor', 'interp', 'EdgeColor', 'none');
-% l1 = light('position', [-1 -1 .1], 'color', [.3 .3 .3]);
-% l2 = light('position', [1 -1 .1], 'color', [.3 .3 .3]);
-% l3 = light('position', [-.5 1 .1], 'color', [.3 .3 .3]);
-% l4 = light;
-% material([.3 .4 .2 10]); lighting phong;
-% axis equal; view(3); ax = axis; title(strrep(name,'_','-'));
-% 
-% subplot(2,2,2); [vertices, faces] = gensfdata(Y);
-% patches = patch('faces', faces, 'vertices', vertices, ...
-% 			'FaceVertexCData', ones(size(vertices,1),1)*[0.8 0.8 0.8], ...
-% 			'FaceColor', 'interp', 'EdgeColor', 'none');
-% l1 = light('position', [-1 -1 .1], 'color', [.3 .3 .3]);
-% l2 = light('position', [1 -1 .1], 'color', [.3 .3 .3]);
-% l3 = light('position', [-.5 1 .1], 'color', [.3 .3 .3]);
-% l4 = light;
-% material([.3 .4 .2 10]); lighting phong;
-% axis equal; view(3); axis(ax);
-% 
-% if ~isempty(find(Y-X==1))
-% 	subplot(2,2,3); [vertices, faces] = gensfdata(Y-X);
-% 	patches = patch('faces', faces, 'vertices', vertices, ...
-% 				'FaceVertexCData', ones(size(vertices,1),1)*[0.8 0.8 0.8], ...
-% 				'FaceColor', 'interp', 'EdgeColor', 'none');
-% 	l1 = light('position', [-1 -1 .1], 'color', [.3 .3 .3]);
-% 	l2 = light('position', [1 -1 .1], 'color', [.3 .3 .3]);
-% 	l3 = light('position', [-.5 1 .1], 'color', [.3 .3 .3]);
-% 	l4 = light;
-% 	material([.3 .4 .2 10]); lighting phong;
-% 	axis equal; view(3); axis(ax); 
-% end
+if ~isdeployed
+    figure;
+
+    subplot(2,2,1); [vertices, faces] = gensfdata(X);
+    patches = patch('faces', faces, 'vertices', vertices, ...
+                'FaceVertexCData', ones(size(vertices,1),1)*[0.8 0.8 0.8], ...
+                'FaceColor', 'interp', 'EdgeColor', 'none');
+    l1 = light('position', [-1 -1 .1], 'color', [.3 .3 .3]);
+    l2 = light('position', [1 -1 .1], 'color', [.3 .3 .3]);
+    l3 = light('position', [-.5 1 .1], 'color', [.3 .3 .3]);
+    l4 = light;
+    material([.3 .4 .2 10]); lighting phong;
+    axis equal; view(3); ax = axis; title(strrep(name,'_','-'));
+
+    subplot(2,2,2); [vertices, faces] = gensfdata(Y);
+    patches = patch('faces', faces, 'vertices', vertices, ...
+                'FaceVertexCData', ones(size(vertices,1),1)*[0.8 0.8 0.8], ...
+                'FaceColor', 'interp', 'EdgeColor', 'none');
+    l1 = light('position', [-1 -1 .1], 'color', [.3 .3 .3]);
+    l2 = light('position', [1 -1 .1], 'color', [.3 .3 .3]);
+    l3 = light('position', [-.5 1 .1], 'color', [.3 .3 .3]);
+    l4 = light;
+    material([.3 .4 .2 10]); lighting phong;
+    axis equal; view(3); axis(ax);
+
+    if ~isempty(find(Y-X==1))
+        subplot(2,2,3); [vertices, faces] = gensfdata(Y-X);
+        patches = patch('faces', faces, 'vertices', vertices, ...
+                    'FaceVertexCData', ones(size(vertices,1),1)*[0.8 0.8 0.8], ...
+                    'FaceColor', 'interp', 'EdgeColor', 'none');
+        l1 = light('position', [-1 -1 .1], 'color', [.3 .3 .3]);
+        l2 = light('position', [1 -1 .1], 'color', [.3 .3 .3]);
+        l3 = light('position', [-.5 1 .1], 'color', [.3 .3 .3]);
+        l4 = light;
+        material([.3 .4 .2 10]); lighting phong;
+        axis equal; view(3); axis(ax); 
+    end
+end
 
 obj = Y(3:end-2,3:end-2,3:end-2); 
 
